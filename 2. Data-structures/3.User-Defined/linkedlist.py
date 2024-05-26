edfb51a9ef6ad8716c4b5abcaaf4b35ed8f9942f
nrrarnn <nurarini0302@gmail.com>
2024-05-26 17:22:04 +0700

    linked list concept

---
+++ 2. Data-structures/3.User-Defined/linkedlist.py
@@ -0,0 +1,25 @@
+# Linked List
+
+# A linked list is a data structure which can change during execution
+# it is a collection of linked nodes(think of a chain)
+# it can be made as long as possible, grow and shrink during execution
+# it does not waste the memory
+
+
+# type of linked List
+
+# singly linked list
+
+# a set of nodes, each containing some data and a link to the next node
+
+# singly list component
+
+# node : self referencing structure
+# list: reference to a node
+# linked list : list of nodes
+#  ---has dedicated head and tail indicators
+
+
+
+
+
