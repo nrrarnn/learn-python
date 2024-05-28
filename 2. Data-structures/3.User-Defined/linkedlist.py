dbce9822328f49cbef8ea2e9227cf0c9c14b7c86
nrrarnn <nurarini0302@gmail.com>
2024-05-28 07:03:21 +0700

    linked list

--- 2. Data-structures/3.User-Defined/linkedlist.py
+++ 2. Data-structures/3.User-Defined/linkedlist.py
@@ -19,7 +19,21 @@
 # linked list : list of nodes
 #  ---has dedicated head and tail indicators
 
+# Create a singly linked list
+# class LinkedList:
 
+class LinkedList:
+  def __init__(self):
+    self.head =  None
+    
+  def print_LL(self):
+    if self.head is None:
+      print("linkedlist is empty!")
+    else:
+      n = self.head
+      while n is not None:
+        print(n.data)
+        n = n.ref
 
 
 
